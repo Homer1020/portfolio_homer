4d780cd80250079f7baa2424f5c68e5da7e09272
Homer <homermoncallo@gmail.com>
2024-04-07 10:21:26 -0400

    Added close button

--- src/utils/modal.js
+++ src/utils/modal.js
@@ -2,7 +2,7 @@ export const generateProjectTemplate = ({title, image, description, stacks, gith
   return `
     <img class="banner" src="${image}" alt='Gomishot'>
     <div>
-      <h1 class="title">${title || ''}</h1>
+      <h2 class="title">${title || ''}</h2>
       <h3 class="subtitle">Descripción:</h3>
       <div>${description || '<p>Inserte contenido aqui</p>'}</div>
       <h3 class="subtitle">Tecnologías:</h3>
@@ -27,4 +27,40 @@ export const generateProjectTemplate = ({title, image, description, stacks, gith
       </div>
     </div>
   `
+}
+
+const handleCloseModal = (e) => {
+  let $target = e.target
+
+  if($target.classList.contains('modal') || $target.classList.contains('modal__close')) {
+    document.body.style.overflow = ''
+    while(!$target.classList.contains('modal')) {
+      console.log('Executing')
+      $target = $target.parentElement
+    }
+    $target.removeEventListener('click', handleCloseModal)
+    $target.remove()
+  }
+}
+
+export const generateModal = (project) => {
+  const $modal = document.createElement('div')
+  $modal.classList.add('modal')
+
+  $modal.innerHTML = `
+    <article class="modal__content">
+      <div class="modal__body">
+        <button class="modal__close">
+          <i class="fa fa-times"></i>
+        </button>
+        ${generateProjectTemplate(project)}
+      </div>
+    </article>
+  `.trim()
+
+  $modal.addEventListener('click', handleCloseModal)
+
+  document.body.style.overflow = 'hidden'
+
+  return $modal
 }
